3f36ee9439782e257f3c5516de7347650719b8c0
lmw17009 <lmw.kuer@gmail.com>
2020-11-16 21:04:29 +0800

    添加非acco的case 语句

--- pedmap/XlsFileAdjust.pas
+++ pedmap/XlsFileAdjust.pas
@@ -3,15 +3,14 @@ unit XlsFileAdjust;
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
-  System.Classes, Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs,
-  Vcl.ComCtrls, Winapi.ShellAPI, FireDAC.Stan.Intf, FireDAC.Stan.Option,
-  FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def,
-  FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.Stan.Param,
-  FireDAC.DatS, FireDAC.DApt.Intf, FireDAC.DApt, FireDAC.VCLUI.Wait,
-  FireDAC.Phys.ODBCDef, FireDAC.Phys.ODBCBase, FireDAC.Phys.ODBC,
-  FireDAC.Comp.UI, Data.DB, FireDAC.Comp.DataSet, FireDAC.Comp.Client,
-  Vcl.StdCtrls, Vcl.Menus, RegularExpressions, System.StrUtils;
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes,
+  Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ComCtrls, Winapi.ShellAPI,
+  FireDAC.Stan.Intf, FireDAC.Stan.Option, FireDAC.Stan.Error, FireDAC.UI.Intf,
+  FireDAC.Phys.Intf, FireDAC.Stan.Def, FireDAC.Stan.Pool, FireDAC.Stan.Async,
+  FireDAC.Phys, FireDAC.Stan.Param, FireDAC.DatS, FireDAC.DApt.Intf, FireDAC.DApt,
+  FireDAC.VCLUI.Wait, FireDAC.Phys.ODBCDef, FireDAC.Phys.ODBCBase, FireDAC.Phys.ODBC,
+  FireDAC.Comp.UI, Data.DB, FireDAC.Comp.DataSet, FireDAC.Comp.Client, Vcl.StdCtrls,
+  Vcl.Menus, RegularExpressions, System.StrUtils;
 
 const
   XlsDiffTableName = 'Counter$';
@@ -181,40 +180,47 @@ begin
         Conn1.Connected := True;
         Qry1 := TFDQuery.Create(Self);
         Qry1.Connection := Conn1;
+        case PedXlsList[I].TablesCount of
+          2:
+            begin
+            //edit data sheet & counter sheet
+              for J := 0 to 1 do
+              begin
+                if PedXlsList[I].Tables[J] <> 'Counter' then
+                begin
+                  NormalTable := PedXlsList[I].Tables[J];
+                end;
+              end;
+              if PedXlsList[I].PPID <> NAPPID then
+              begin
+          //change ppid
+                with Qry1 do
+                begin
+                  Close;
+                  SQL.Clear;
+                  StrSql := 'update [' + NormalTable + '] set F2=''' + NAPPID + ''' where F2=''' + PedXlsList[I].PPID + '''';
+                  SQL.Text := StrSql;
+                  ExecSQL;
+                end;
+              end;
+        //change lotid
+              if PedXlsList[I].LotID <> NALotID then
+              begin
+          //change ppid
+                with Qry1 do
+                begin
+                  Close;
+                  SQL.Clear;
+                  SQL.Text := 'update [' + NormalTable + '] set F7=''' + NALotID + ''' where F7=''' + PedXlsList[I].LotID + '''';
+                  ExecSQL;
+                end;
+              end;
+
+            end;
+
+        end;
         if PedXlsList[I].TablesCount = 2 then
         begin
-          //edit data sheet & counter sheet
-          for J := 0 to 1 do
-          begin
-            if PedXlsList[I].Tables[J] <> 'Counter' then
-            begin
-              NormalTable := PedXlsList[I].Tables[J];
-            end;
-          end;
-          if PedXlsList[I].PPID <> NAPPID then
-          begin
-          //change ppid
-            with Qry1 do
-            begin
-              Close;
-              SQL.Clear;
-              StrSql := 'update [' + NormalTable + '] set F2=''' + NAPPID + ''' where F2=''' + PedXlsList[I].PPID + '''';
-              SQL.Text := StrSql;
-              ExecSQL;
-            end;
-          end;
-        //change lotid
-          if PedXlsList[I].LotID <> NALotID then
-          begin
-          //change ppid
-            with Qry1 do
-            begin
-              Close;
-              SQL.Clear;
-              SQL.Text := 'update [' + NormalTable + '] set F7=''' + NALotID + ''' where F7=''' + PedXlsList[I].LotID + '''';
-              ExecSQL;
-            end;
-          end;
 
         end;
 
